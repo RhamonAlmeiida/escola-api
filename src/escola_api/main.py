5ff192dcc4a1025ed43b7fec6c8b7b82d659f0e2
RhamonAlmeiida <rhamonalmeida.dev@hotmail.com>
2025-05-25 21:38:06 -0300

    finalizado crud aluno

--- src/escola_api/main.py
+++ src/escola_api/main.py
@@ -100,5 +100,78 @@ def editar_curso(id: int, form: CursoEditar):
     raise HTTPException(status_code=404, detail=f"curso não encontrado com id : {id}")
 
 
+
+## AREA ALUNOS
+
+@dataclass
+class Aluno:
+    id: int = field()
+    nome: str = field()
+    sobrenome: str = field()
+    cpf: str = field()
+    data_nascimento: str = field()
+
+@dataclass
+class AlunoCadastro:
+    nome: str = field()
+    sobrenome: str = field()
+    cpf: str = field()
+    data_nascimento: str = field()
+
+@dataclass
+class AlunoEditar:
+    nome: str = field()
+    sobrenome: str = field()
+    cpf: str = field()
+    data_nascimento: str = field()
+
+alunos = [
+    # instanciando um objeto da classe Curso
+    Aluno( id = 1, nome = "Rhamon", sobrenome= "Almeida", cpf="161.074.517-57", data_nascimento = "1996-04-23"),
+    Aluno(id=2, nome="Larissa", sobrenome="Menezes", cpf="432.781.229-30", data_nascimento="1998-11-15"),
+    Aluno(id=3, nome="Carlos", sobrenome="Souza", cpf="205.467.880-92", data_nascimento="2001-06-09"),
+]
+
+@app.get("/api/alunos")
+def listar_todos_alunos():
+    return alunos
+
+@app.get("/api/alunos/{id}")
+def obter_aluno(id: int):
+    for aluno in alunos:
+        if aluno.id == id:
+            return aluno
+    raise HTTPException(status_code=404, detail=f"Curso não encontrado com id: {id}")
+
+
+@app.post("/api/alunos")
+def cadastrar_aluno(form: AlunoCadastro):
+    ultimo_id = max([aluno.id for aluno in alunos], default=0)
+
+    aluno = Aluno(id = ultimo_id + 1, nome=form.nome, sobrenome= form.sobrenome, cpf=form.cpf, data_nascimento = form.data_nascimento)
+
+    alunos.append(aluno)
+    return aluno
+
+@app.put("/api/alunos/{id}")
+def editar_aluno(form: AlunoEditar):
+    for aluno in alunos:
+        if aluno.id == id:
+            aluno.nome = form.nome
+            aluno.sobrenome = form.sobrenome
+            aluno.cpf = form.cpf
+            aluno.data_nascimento = form.data_nascimento
+            return aluno
+    raise HTTPException(status_code=404, detail=f"curso não encontrado com id : {id}")
+
+@app.delete("/api/alunos/{id}")
+def apagar_aluno(id: int):
+    for aluno in alunos:
+        if aluno.id == id:
+            alunos.remove(aluno)
+            return
+    raise HTTPException(status_code=404, detail=f"curso não encontrado com id : {id}")
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app")
