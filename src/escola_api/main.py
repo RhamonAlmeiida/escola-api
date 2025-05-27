36aec8927c857caf1ac08ecda0fba324f9a62e43
Rhamon Silva Almeida <75868@proway.treina>
2025-05-26 21:53:39 -0300

    finalizado crud aluno

--- src/escola_api/main.py
+++ src/escola_api/main.py
@@ -1,10 +1,25 @@
+from calendar import month
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import datetime, date
 
 import uvicorn
+from dataclasses_json import dataclass_json, LetterCase
 from fastapi import FastAPI, HTTPException
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def index():
@@ -102,34 +117,38 @@ def editar_curso(id: int, form: CursoEditar):
 
 
 ## AREA ALUNOS
-
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Aluno:
     id: int = field()
     nome: str = field()
     sobrenome: str = field()
     cpf: str = field()
-    data_nascimento: str = field()
+    data_nascimento: date = field()
 
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class AlunoCadastro:
     nome: str = field()
     sobrenome: str = field()
     cpf: str = field()
-    data_nascimento: str = field()
+    data_nascimento: date = field()
 
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class AlunoEditar:
     nome: str = field()
     sobrenome: str = field()
     cpf: str = field()
-    data_nascimento: str = field()
+    data_nascimento: date = field()
 
 alunos = [
     # instanciando um objeto da classe Curso
-    Aluno( id = 1, nome = "Rhamon", sobrenome= "Almeida", cpf="161.074.517-57", data_nascimento = "1996-04-23"),
-    Aluno(id=2, nome="Larissa", sobrenome="Menezes", cpf="432.781.229-30", data_nascimento="1998-11-15"),
-    Aluno(id=3, nome="Carlos", sobrenome="Souza", cpf="205.467.880-92", data_nascimento="2001-06-09"),
+    Aluno( id = 1, nome = "Rhamon", sobrenome= "Almeida", cpf="161.074.517-57", data_nascimento = date(1996,4,23)),
+    Aluno(id=2, nome="Larissa", sobrenome="Menezes", cpf="432.781.229-30", data_nascimento = date(1999,10,18)),
+    Aluno(id=3, nome="Carlos", sobrenome="Souza", cpf="205.467.880-92", data_nascimento = date(2006,4,12)),
 ]
 
 @app.get("/api/alunos")
@@ -141,7 +160,7 @@ def obter_aluno(id: int):
     for aluno in alunos:
         if aluno.id == id:
             return aluno
-    raise HTTPException(status_code=404, detail=f"Curso não encontrado com id: {id}")
+    raise HTTPException(status_code=404, detail=f"Aluno não encontrado com id: {id}")
 
 
 @app.post("/api/alunos")
@@ -154,7 +173,7 @@ def cadastrar_aluno(form: AlunoCadastro):
     return aluno
 
 @app.put("/api/alunos/{id}")
-def editar_aluno(form: AlunoEditar):
+def editar_aluno(id: int, form: AlunoEditar):
     for aluno in alunos:
         if aluno.id == id:
             aluno.nome = form.nome
@@ -162,7 +181,7 @@ def editar_aluno(form: AlunoEditar):
             aluno.cpf = form.cpf
             aluno.data_nascimento = form.data_nascimento
             return aluno
-    raise HTTPException(status_code=404, detail=f"curso não encontrado com id : {id}")
+    raise HTTPException(status_code=404, detail=f"Aluno não encontrado com id : {id}")
 
 @app.delete("/api/alunos/{id}")
 def apagar_aluno(id: int):
@@ -170,7 +189,7 @@ def apagar_aluno(id: int):
         if aluno.id == id:
             alunos.remove(aluno)
             return
-    raise HTTPException(status_code=404, detail=f"curso não encontrado com id : {id}")
+    raise HTTPException(status_code=404, detail=f"Aluno não encontrado com id : {id}")
 
 
 if __name__ == "__main__":
