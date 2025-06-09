09592f0532f06d8aed25ca8244d2d86c33ebcd3f
Rhamon Silva Almeida <75868@PROWAY.TREINA>
2025-06-09 20:26:54 -0300

    Finalizado crud Matriculas

--- src/escola_api/main.py
+++ src/escola_api/main.py
@@ -1,15 +1,20 @@
 import uvicorn
 
+from escola_api.api.v1 import matricula_controller
+from escola_api.database.banco_dados import popular_banco_dados
 from src.escola_api.api.v1 import aluno_controller
 from src.escola_api.database.banco_dados import engine, Base
 from src.escola_api.api.v1 import curso_controller
 from src.escola_api.app import app
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+popular_banco_dados()
 
 
 app.include_router(curso_controller.router)
 app.include_router(aluno_controller.router)
+app.include_router(matricula_controller.router)
 
 
 if __name__ == "__main__":
